d09109b9aa36ae3a62f57e6dbc9425d8e6c57c9d
Olena Ysynetska <olenayasynetska8@gmail.com>
2025-07-30 13:56:39 +0200

    backend

--- src/shared/api/config.js
+++ src/shared/api/config.js
@@ -1,7 +1,8 @@
 // Конфигурация API
-export const API_BASE_URL = 'http://localhost:3333';
+// export const API_BASE_URL = 'http://localhost:3333';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-// Функция для создания полного URL эндпоинта
+// Функция для создания полного URL
 export const createApiUrl = (endpoint) => {
   return `${API_BASE_URL}${endpoint}`;
 };
